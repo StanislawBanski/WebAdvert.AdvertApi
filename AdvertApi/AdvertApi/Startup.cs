3b404887238e0b480ed3f1fdd416b752c2dfca82
Stanisław Bański <staban95@gmail.com>
2020-03-14 09:11:32 +0100

    Add CloudWatch register for discovery

--- AdvertApi/AdvertApi/Startup.cs
+++ AdvertApi/AdvertApi/Startup.cs
@@ -1,12 +1,18 @@
 using AdvertApi.HealthChecks;
 using AdvertApi.Interfaces;
 using AdvertApi.Services;
+using Amazon.ServiceDiscovery;
+using Amazon.ServiceDiscovery.Model;
+using Amazon.Util;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AdvertApi
 {
@@ -41,7 +47,7 @@ namespace AdvertApi
             });
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async Task Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -59,10 +65,32 @@ namespace AdvertApi
                 action.SwaggerEndpoint("/swagger/v1/swagger.json", "Web Advert Api");
             });
 
+            await RegisterToCloudMap();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private async Task RegisterToCloudMap()
+        {
+            var instanceId = EC2InstanceMetadata.InstanceId;
+            string serviceId = Configuration.GetValue<string>("ServiceId");
+
+            if (!string.IsNullOrEmpty(instanceId))
+            {
+                var ipv4 = EC2InstanceMetadata.PrivateIpAddress;
+                var client = new AmazonServiceDiscoveryClient(Amazon.RegionEndpoint.EUWest2);
+
+                await client.RegisterInstanceAsync(new RegisterInstanceRequest
+                {
+                    ServiceId = serviceId,
+                    InstanceId = instanceId,
+                    Attributes = new Dictionary<string, string>() { {"AWS_INSTANCE_IPV4", ipv4 }, { "AWS_INSTANCE_PORT", "80" } }
+                });
+                
+            }
         }
     }
 }
